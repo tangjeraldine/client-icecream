e9c987d088f5776828d8fae9034d0e8a50f093b7
tangjeraldine <typjera.1127@gmail.com>
2022-10-03 16:47:15 +0800

    amended url

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,45 @@ import Search from "./Routes/Search";
 
 const AllIceCreamLoader = async () => {
   try {
-    const res = await fetch("http://localhost:3000/route/allicecream");
+    const res = await fetch(
+      "https://unusual-slug-pullover.cyclic.app/route/allicecream"
+    );
     const data = await res.json();
-    // console.log(data);
+    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-const OneIceCreamLoader = async () => {};
+const OneIceCreamLoader = async () => {
+  try {
+    const res = await fetch(
+      "https://unusual-slug-pullover.cyclic.app/route/search",
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const addNewAction = async ({ request }) => {
   try {
     const form = Object.fromEntries(await request.formData());
-    const res = await fetch("http://localhost:3000/route/newicecream", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form),
-    });
+    const res = await fetch(
+      "https://unusual-slug-pullover.cyclic.app/route/newicecream",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      }
+    );
     return redirect("/");
   } catch (error) {
     console.log("error", error);
@@ -46,15 +66,7 @@ const deleteAction = async () => {
   console.log("deleted");
 };
 
-const searchAction = async ({ request }) => {
-  // try {
-  const search = await request.formData();
-  console.log(search.get("name"));
-  // const res = await fetch();
-  // } catch (error) {
-  //   console.log("error", error);
-  // }
-};
+const searchAction = async () => {};
 
 const router = createBrowserRouter([
   {
